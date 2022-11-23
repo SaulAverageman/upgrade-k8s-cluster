1f789219ac5f55369d7ddb1fe18b01a693f3f93c
anonie <mail@anymail.com>
2022-11-23 17:04:02 +0530

    add code stuff

--- upgrade-control-plane.sh
+++ upgrade-control-plane.sh
@@ -5,10 +5,10 @@ NEW-K8S-VER=1.25.4
 echo "upgrading kubeadm"
 apt-mark unhold kubeadm && \
 apt-get update && apt-get install -y kubeadm={NEW-K8S-VER}-00 && \
-apt-mark hold kubeadm
-sudo kubeadm upgrade apply v${NEW-K8S-VER}
+apt-mark hold kubeadm && \
+sudo kubeadm upgrade apply v${NEW-K8S-VER} && \
 
-sh upgrade-kubectl-kubeadm.sh
+sh upgrade-kubectl-kubeadm.sh && \
 
 #upgrade nodes
 for node in $(kubectl get nodes -o name)
@@ -18,9 +18,9 @@ do
 	then
 		continue
 	fi
-	kubectl cordon ${node}
-	kubectl drain ${node} --ignore-daemonsets
-	ssh azureuser@${node} 'bash -s' < upgrade-kubectl-kubeadm.sh
+	kubectl cordon ${node} && \
+	kubectl drain ${node} --ignore-daemonsets && \
+	ssh azureuser@${node} 'bash -s' < upgrade-kubectl-kubeadm.sh && \
 	kubectl uncordon ${node}
 done
  
--- upgrade-kubectl-kubeadm.sh
+++ upgrade-kubectl-kubeadm.sh
@@ -5,7 +5,7 @@ NEW-K8S-VER=1.25.4
 echo "upgrading kubectl and kubelet"
 apt-mark unhold kubelet kubectl && \
 apt-get update && apt-get install -y kubelet={NEW-K8S-VER}-00 kubectl={NEW-K8S-VER}-00 && \
-apt-mark hold kubelet kubectl
-sudo systemctl daemon-reload
-sudo systemctl restart kubelet
+apt-mark hold kubelet kubectl && \
+sudo systemctl daemon-reload && \
+sudo systemctl restart kubelet && \
 sudo kubeadm upgrade node
