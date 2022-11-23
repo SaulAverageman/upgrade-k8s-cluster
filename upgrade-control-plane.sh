11b84ee24bc878d3b9987c8567cdb917db1a103e
anonie <mail@anymail.com>
2022-11-23 19:37:06 +0530

    add code stuff

--- upgrade-control-plane.sh
+++ upgrade-control-plane.sh
@@ -2,12 +2,6 @@
 
 export NEW_K8S_VER=1.25.3
 
-echo "upgrading kubeadm"
-sudo apt-mark unhold kubeadm && \
-apt-get update && apt-get install -y kubeadm=${NEW_K8S_VER}-00 && \
-sudo apt-mark hold kubeadm && \
-sudo kubeadm upgrade apply v${NEW_K8S_VER} --ignore-preflight-errors=all && \
-
 sh upgrade-kubectl-kubeadm.sh && \
 
 #upgrade nodes
--- upgrade-kubectl-kubeadm.sh
+++ upgrade-kubectl-kubeadm.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo "upgrading kubectl and kubelet"
+echo "upgrading kubeadm"
+sudo apt-mark unhold kubeadm && \
+apt-get update && apt-get install -y kubeadm=${NEW_K8S_VER}-00 && \
+sudo kubeadm upgrade apply v${NEW_K8S_VER} --ignore-preflight-errors=all && \
+
+echo "Bubai says-upgrading kubectl and kubelet"
 sudo apt-mark unhold kubelet kubectl && \
 apt-get update && apt-get install -y kubelet=${NEW_K8S_VER}-00 kubectl=${NEW_K8S_VER}-00 && \
-sudo apt-mark hold kubelet kubectl && \
+sudo kubeadm upgrade node --ignore-preflight-errors=all && \
 sudo systemctl daemon-reload && \
-sudo systemctl restart kubelet && \
-sudo kubeadm upgrade node --ignore-preflight-errors=all
+sudo systemctl restart kubelet
