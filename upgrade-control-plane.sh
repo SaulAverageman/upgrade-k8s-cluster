db84a03ec369e213115eac8e2acc8d3e3bc826fa
anonie <mail@anymail.com>
2022-11-23 15:42:05 +0530

    add code stuff

--- upgrade-control-plane.sh
+++ upgrade-control-plane.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+NEW-K8S-VER=1.25.4
+
 echo "upgrading kubeadm"
 apt-mark unhold kubeadm && \
-apt-get update && apt-get install -y kubeadm=1.24.6-00 && \
+apt-get update && apt-get install -y kubeadm={NEW-K8S-VER}-00 && \
 apt-mark hold kubeadm
 sudo kubeadm upgrade apply v1.24.6
 
@@ -10,6 +13,7 @@ sh upgrade-kubectl-kubeadm.sh
 #upgrade nodes
 for node in $(kubectl get nodes -o name)
 do
+	node=${node##*/}
 	kubectl cordon ${node}
 	kubectl drain ${node} --ignore-daemonsets
 	ssh azureuser@${node} 'bash -s' < upgrade-kubectl-kubeadm.sh
--- upgrade-kubectl-kubeadm.sh
+++ upgrade-kubectl-kubeadm.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+NEW-K8S-VER=1.25.4
+
 echo "upgrading kubectl and kubelet"
 apt-mark unhold kubelet kubectl && \
-apt-get update && apt-get install -y kubelet=1.24.6-00 kubectl=1.24.6-00 && \
+apt-get update && apt-get install -y kubelet={NEW-K8S-VER}-00 kubectl={NEW-K8S-VER}-00 && \
 apt-mark hold kubelet kubectl
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
