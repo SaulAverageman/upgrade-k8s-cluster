5005a6d767ab6583224c108cdb592cebaf503e04
anonie <mail@anymail.com>
2022-11-23 17:11:25 +0530

    add code stuff

--- upgrade-control-plane.sh
+++ upgrade-control-plane.sh
@@ -4,9 +4,9 @@ export NEW_K8S_VER=1.25.4
 
 echo "upgrading kubeadm"
 sudo apt-mark unhold kubeadm && \
-apt-get update && apt-get install -y kubeadm={NEW-K8S-VER}-00 && \
+apt-get update && apt-get install -y kubeadm={NEW_K8S_VER}-00 && \
 sudo apt-mark hold kubeadm && \
-sudo kubeadm upgrade apply v${NEW-K8S-VER} && \
+sudo kubeadm upgrade apply v${NEW_K8S_VER} && \
 
 sh upgrade-kubectl-kubeadm.sh && \
 
--- upgrade-kubectl-kubeadm.sh
+++ upgrade-kubectl-kubeadm.sh
@@ -2,7 +2,7 @@
 
 echo "upgrading kubectl and kubelet"
 sudo apt-mark unhold kubelet kubectl && \
-apt-get update && apt-get install -y kubelet={NEW-K8S-VER}-00 kubectl={NEW-K8S-VER}-00 && \
+apt-get update && apt-get install -y kubelet={NEW_K8S_VER}-00 kubectl={NEW_K8S_VER}-00 && \
 sudo apt-mark hold kubelet kubectl && \
 sudo systemctl daemon-reload && \
 sudo systemctl restart kubelet && \
