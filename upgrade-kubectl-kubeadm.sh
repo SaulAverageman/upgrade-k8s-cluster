893a9df7e632e0c39d2d5ef618b3136ff40e0d8e
anonie <mail@anymail.com>
2022-11-23 19:53:45 +0530

    add code stuff

--- upgrade-kubectl-kubeadm.sh
+++ upgrade-kubectl-kubeadm.sh
@@ -4,11 +4,11 @@ export NEW_K8S_VER=1.25.3
 
 echo "upgrading kubeadm"
 sudo apt-mark unhold kubeadm && \
-apt-get update && apt-get install -y kubeadm=${NEW_K8S_VER}-00 && \
+sudo apt-get update && sudo apt-get install -y kubeadm=${NEW_K8S_VER}-00 && \
 sudo kubeadm upgrade apply v${NEW_K8S_VER} --ignore-preflight-errors=all && \
 
 echo "Bubai says-upgrading kubectl and kubelet"
 sudo apt-mark unhold kubelet kubectl && \
-apt-get update && apt-get install -y kubelet=${NEW_K8S_VER}-00 kubectl=${NEW_K8S_VER}-00 && \
+sudo apt-get update && sudo apt-get install -y kubelet=${NEW_K8S_VER}-00 kubectl=${NEW_K8S_VER}-00 && \
 sudo systemctl daemon-reload && \
 sudo systemctl restart kubelet
