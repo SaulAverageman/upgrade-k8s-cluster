ce44080ce930d439bbb8cc73b323dc02acf78b01
SaulAverageman <86016590+SaulAverageman@users.noreply.github.com>
2022-11-25 13:54:39 +0530

    Update upgrade-kubectl-kubeadm.sh

--- upgrade-kubectl-kubeadm.sh
+++ upgrade-kubectl-kubeadm.sh
@@ -7,7 +7,7 @@ sudo apt-mark unhold kubeadm && \
 sudo apt-get update && sudo apt-get install -y kubeadm=${NEW_K8S_VER}-00 && \
 sudo kubeadm upgrade apply v${NEW_K8S_VER} --ignore-preflight-errors=all && \
 
-echo "Bubai says-upgrading kubectl and kubelet"
+echo "Saul says-upgrading kubectl and kubelet"
 sudo apt-mark unhold kubelet kubectl && \
 sudo apt-get update && sudo apt-get install -y kubelet=${NEW_K8S_VER}-00 kubectl=${NEW_K8S_VER}-00 && \
 sudo systemctl daemon-reload && \
